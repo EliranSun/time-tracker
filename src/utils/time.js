d88fa97cdbce06ea390934e6c80b6f9dcfc417ff
Eliran Shemesh <piro27@gmail.com>
2024-02-20 08:03:35 +0200

    formatTimestamp util

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,3 +1,13 @@
-export const formatTime = (hours, minutes, seconds) => {
+export const formatTimestamp = (timestamp) => {
+    const hours = Math.floor(counter / 3600);
+    const minutes = Math.floor((counter - hours * 3600) / 60);
+    const seconds = counter - hours * 3600 - minutes * 60;
+
+    let timeString = "";
     
+    if (hours > 0) timeString += hours;
+    if (minutes > 0) timeString += `:{minutes}`;
+    if (seconds > 0) timeString += `:{seconds}`;
+
+    return timeString;
 };
