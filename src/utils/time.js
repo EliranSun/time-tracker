20114effedf41f6c293c306c989d216c227358a6
Eliran Shemesh <piro27@gmail.com>
2024-02-20 09:22:02 +0200

    Format less than 10

--- src/utils/time.js
+++ src/utils/time.js
@@ -8,8 +8,16 @@ export const formatTimestamp = (timestampDiff) => {
     const seconds = Math.floor((durationSeconds - hours * ONE_HOUR - minutes * 60));
     
     const hoursString = hours || "00";
-    const minutesString = minutes ? `:${minutes}` : ":00";
-    const secondsString = seconds ? `:${seconds}` : ":00";
+    let minutesString = ":00";
+    let secondsString = ":00";
 
+    if (minutes > 0)
+        if (minutes < 10) minutesString = `:0${minutes}`;
+        else minutesString = `:${minutes}`;
+        
+    if (seconds > 0)
+        if (seconds < 10) secondsString = `:0${seconds}`;
+        else secondsString = `:${seconds}`;
+        
     return hoursString + minutesString + secondsString;;
 };
