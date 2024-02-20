44f69c8497872188f3dba792bf1914e664376a89
Eliran Shemesh <piro27@gmail.com>
2024-02-20 08:58:09 +0200

    Fix extra division

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,7 @@
 export const formatTimestamp = (timestamp) => {
-    const hours = Math.floor(timestamp / 24 / 60 / 60 / 1000);
-    const minutes = Math.floor(timestamp / 60 / 60 / 1000);
-    const seconds = Math.floor(timestamp / 60 / 1000);
+    const hours = Math.floor(timestamp / 24 / 60 / 1000);
+    const minutes = Math.floor(timestamp / 60 / 1000);
+    const seconds = Math.floor(timestamp / 1000);
     
     const hoursString = hours || "00";
     const minutesString = minutes ? `:${minutes}` : ":00";
