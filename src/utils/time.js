910f2c8695a9a3a281baba49843f92f90e1f3f7b
Eliran Shemesh <piro27@gmail.com>
2024-02-20 08:40:06 +0200

    All calc should be ms

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,7 @@
 export const formatTimestamp = (timestamp) => {
     const hours = Math.floor(timestamp / 360000);
-    const minutes = Math.floor((timestamp - hours * 3600) / 60);
-    const seconds = timestamp - hours * 3600 - minutes * 60;
+    const minutes = Math.floor((timestamp - hours * 360000) / 60);
+    const seconds = timestamp - hours * 360000 - minutes * 60;
     
     const hoursString = hours || "00";
     const minutesString = minutes ? `:${minutes}` : ":00";
