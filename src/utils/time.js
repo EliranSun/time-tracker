981933bd8eb4d311709be77172dec59d3613ca8e
Eliran Shemesh <piro27@gmail.com>
2024-02-20 08:10:15 +0200

    Fix non existing var

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,7 @@
 export const formatTimestamp = (timestamp) => {
-    const hours = Math.floor(counter / 3600);
-    const minutes = Math.floor((counter - hours * 3600) / 60);
-    const seconds = counter - hours * 3600 - minutes * 60;
+    const hours = Math.floor(timestamp / 3600);
+    const minutes = Math.floor((timestamp - hours * 3600) / 60);
+    const seconds = timestamp - hours * 3600 - minutes * 60;
 
     let timeString = "";
     
