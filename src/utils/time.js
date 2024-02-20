c27fd3151dffcf934f5fdac51664e6b426d10a7c
Eliran Shemesh <piro27@gmail.com>
2024-02-20 08:25:38 +0200

    Return some actual value from the function

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,12 +2,10 @@ export const formatTimestamp = (timestamp) => {
     const hours = Math.floor(timestamp / 3600);
     const minutes = Math.floor((timestamp - hours * 3600) / 60);
     const seconds = timestamp - hours * 3600 - minutes * 60;
-
-    let timeString = "";
     
     const hoursString = hours || "00";
     const minutesString = minutes ? `:${minutes}` : ":00";
     const secondsString = seconds ? `:${seconds}` : ":00";
 
-    return timeString;
+    return hoursString + minutesString + secondsString;;
 };
