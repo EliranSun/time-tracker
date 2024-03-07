b0b37644345a5c9b190415ac5f2ceea6cdb54331
Eliran Shemesh <piro27@gmail.com>
2024-03-07 18:09:39 +0200

    Constant height + below

--- src/components/LastWeekDataStrip.jsx
+++ src/components/LastWeekDataStrip.jsx
@@ -3,7 +3,7 @@ import {getLastWeekData} from "../utils/session";
 
 const Duration = ({ item }) => {
     return (
-        <p className="text-[10px] leading-[9px] flex flex-col m-auto inset-x-0 bottom-1">
+        <p className="h-4 text-[10px] leading-[9px] flex flex-col m-auto inset-x-0 bottom-1">
                                 {item.duration.hours > 0 ? <span>{item.duration.hours}h</span> : null}
                                 {item.duration.minutes > 0 ? <span>{item.duration.minutes}m</span> : null}
                         </p>
@@ -21,12 +21,12 @@ export const LastWeekDataStrip = ({activity, data}) => {
                 const measure = item.measure || 0;
                 return (
                     <div key={item.name} className="flex flex-col items-center w-7">
-                        <Duration item={item} />
                         <div className="relative flex flex-col flex-wrap gap-0 min-h-7 bg-black dark:bg-white opacity-20">
                             {new Array(measure).fill(null).map((_, index) => {
                                 return <span key={index} className="w-2 h-2 bg-black dark:bg-white"/>
                             })}
                         </div>
+                        <Duration item={item} />
                         <p className="opacity-40">{item.name.slice(0, 1)}</p>
                     </div>
                 )
