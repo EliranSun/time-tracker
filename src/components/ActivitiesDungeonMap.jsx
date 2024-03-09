6745263e72d7d361947876f1fbc0a950d26354c2
Eliran Shemesh <piro27@gmail.com>
2024-03-09 17:01:26 +0200

    Golden border

--- src/components/ActivitiesDungeonMap.jsx
+++ src/components/ActivitiesDungeonMap.jsx
@@ -2,6 +2,8 @@ import { Activities } from "../constants/activities";
 import classNames from "classnames";
 import { Icon } from "./Icon";
 
+const GOLD = "#FFBF00";
+
 export const ActivitiesDungeonMap = ({ activePage, isZenMode, goldenActivity }) => {
     if (isZenMode) {
         return null;
@@ -21,9 +23,10 @@ export const ActivitiesDungeonMap = ({ activePage, isZenMode, goldenActivity }) 
                             className={classNames("w-4 h-4 flex items-center justify-center", {
                                 "bg-gray-500 text-white dark:bg-white dark:text-black opacity-80": isActive,
                                 "opacity-30": !isActive,
+                                "border border-yellow-400": isGolden,
                             })}>
                             <Icon 
-                                color={isGolden && "#FFBF00"}
+                                color={isGolden && GOLD}
                                 weight={isGolden ? "fill" : "regular"}
                                 name={activity.icon}/>
                         </span>
