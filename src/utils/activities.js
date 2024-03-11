9bc91735d4243277553f8deea75e57714b2413b6
Eliran Shemesh <piro27@gmail.com>
2024-03-12 01:14:50 +0200

    storage mock flag

--- src/utils/activities.js
+++ src/utils/activities.js
@@ -3,10 +3,10 @@ import {db} from "./db";
 import allActivitiesMock from '../mocks/all-activities.json';
 
 export const getAllDocsInActivity = async (activityName) => {
-    // if (process.env.NODE_ENV === "development") {
-    //     console.log('saved expensive call');
-    //     return allActivitiesMock;
-    // }
+    if (localStorage.getItem('mock')) {
+        console.log('saved expensive call');
+        return allActivitiesMock;
+    }
 
     // TODO: This is O(n) and should be O(1)
     const querySnapshot = await getDocs(collection(db, `activities/${activityName}/data`));
@@ -15,9 +15,7 @@ export const getAllDocsInActivity = async (activityName) => {
     querySnapshot.forEach((doc) => {
         data.push(doc.data());
     });
-
-
-
+    
     return data;
 };
 
