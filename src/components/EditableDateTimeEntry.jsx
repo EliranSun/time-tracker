a68990e86c7db2ccad02c69e4b363233a6f13bb0
Eliran Shemesh <piro27@gmail.com>
2024-03-14 09:39:14 +0200

    Update toast UI

--- src/components/EditableDateTimeEntry.jsx
+++ src/components/EditableDateTimeEntry.jsx
@@ -12,7 +12,7 @@ const Toast = ({ type }) => {
     return (
         <div className={classNames({
             "fixed rounded flex items-center justify-center left-0 right-0": true, 
-            "m-auto bottom-10 bg-black text-white w-fit px-4 text-lg h-10": true,
+            "m-auto bottom-20 bg-black text-white w-fit px-4 text-lg h-10": true,
             })}>
             { type === "success" ? "alright!" : "something got entangled" }
         </div>
