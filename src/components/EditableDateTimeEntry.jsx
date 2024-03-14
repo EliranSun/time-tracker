670834b6564e1a3223779969b15a5cec54277499
Eliran Shemesh <piro27@gmail.com>
2024-03-14 09:40:48 +0200

    Hide toast and prepare animation

--- src/components/EditableDateTimeEntry.jsx
+++ src/components/EditableDateTimeEntry.jsx
@@ -11,7 +11,8 @@ const onDateTimeChange = (event) => {
 const Toast = ({ type }) => {
     return (
         <div className={classNames({
-            "fixed rounded flex items-center justify-center left-0 right-0": true, 
+            "opacity-0": !type,
+            "animate-all fixed rounded flex items-center justify-center left-0 right-0": true, 
             "m-auto bottom-20 bg-black text-white w-fit px-4 text-lg h-10": true,
             })}>
             { type === "success" ? "alright!" : "something got entangled" }
