9a71e3e939df75df58fa50bf304a1b855b795ef6
Eliran Shemesh <piro27@gmail.com>
2024-03-14 10:02:12 +0200

    Fix animation property name

--- src/components/EditableDateTimeEntry.jsx
+++ src/components/EditableDateTimeEntry.jsx
@@ -13,7 +13,7 @@ const Toast = ({ type }) => {
         <div className={classNames({
             "opacity-0": !type,
             "opacity-100": type,
-            "animate-all fixed rounded flex items-center justify-center left-0 right-0": true, 
+            "transition-all fixed rounded flex items-center justify-center left-0 right-0": true, 
             "m-auto bottom-20 bg-black text-white w-fit px-4 text-lg h-10": true,
             })}>
             { type === "success" ? "alright!" : "something got entangled" }
