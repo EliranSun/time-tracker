c1eb8655df5e5259520075c62caf58ff8fb89111
Eliran Shemesh <piro27@gmail.com>
2024-03-15 09:22:39 +0200

    Wrapper

--- src/components/organisms/EditActivityEntryModal.jsx
+++ src/components/organisms/EditActivityEntryModal.jsx
@@ -2,6 +2,14 @@ import { noop } from "lodash";
 import { X, Check } from "@phosphor-icons/react";
 import { EditableDateTimeEntry } from "../EditableDateTimeEntry";
 
+const ModalButton = ({children}) => {
+    return (
+         <span className="rounded-full border-4 border-black dark:border-white p-4 hover:bg-white hover:text-black">
+            {children}
+        </span>
+    );
+};
+
 export const EditActivityEntryModal = ({ entry, isOpen = false, onClose = noop }) => {
     if (!isOpen)
         return null;
@@ -12,16 +20,21 @@ export const EditActivityEntryModal = ({ entry, isOpen = false, onClose = noop }
                     open={true}
                     className="backdrop-blur-xl fixed z-30 flex items-center bg-transparent justify-center w-screen h-screen top-0">
                     <div className="flex flex-col items-center font-mono justify-center w-5/6 h-2/3 rounded-xl p-8 pt-16">
-                        <span className="absolute top-14 rounded-full border-4 border-black dark:border-white p-4 hover:bg-white hover:text-black">
+                        <div className="absolute top-14">
+                        <ModalButton>
                             <X
                                 size={52}
                                 onClick={onClose}
                                 className="dark:text-white hover:text-black"/>
-                            {!entry.id ? <Check
+                        </ModalButton>
+                         {!entry.id ? 
+                        <ModalButton>
+                           <Check
                                 size={52}
                                 onClick={onClose}
-                                className="dark:text-white hover:text-black"/> : null}
-                        </span>
+                                className="dark:text-white hover:text-black"/> 
+                        </ModalButton>: null}
+                        </div>
                         <h1>{entry.name}</h1>
                         <EditableDateTimeEntry
                             id={entry.id}
