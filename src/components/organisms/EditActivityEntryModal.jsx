ebc08de59c623d89a372de14aa442b1ed78c6d1a
Eliran Shemesh <piro27@gmail.com>
2024-03-15 09:02:47 +0200

    Columns items for title

--- src/components/organisms/EditActivityEntryModal.jsx
+++ src/components/organisms/EditActivityEntryModal.jsx
@@ -11,7 +11,7 @@ export const EditActivityEntryModal = ({ entry, isOpen = false, onClose = noop }
                     onClose={onClose}
                     open={true}
                     className="backdrop-blur-xl fixed z-30 flex items-center bg-transparent justify-center w-screen h-screen top-0">
-                    <div className="flex items-center font-mono justify-center w-5/6 h-2/3 rounded-xl p-8 pt-16">
+                    <div className="flex flex-col items-center font-mono justify-center w-5/6 h-2/3 rounded-xl p-8 pt-16">
                         <span className="absolute top-14 rounded-full border-4 border-black dark:border-white p-4 hover:bg-white hover:text-black">
                             <X
                                 size={52}
