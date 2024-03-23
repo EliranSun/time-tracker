450f842447e7d447f7404eee71e67a0276110e5e
Eliran Shemesh <piro27@gmail.com>
2024-03-23 17:27:36 +0200

    Adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,8 +48,8 @@ export const Header = ({
             </div>
             <div
                 className={classNames({
-                    "w-2/3 flex items-center justify-center gap-8": true,
-                    "text-xl px-4 py-2 rounded-2xl": hasBackgroundActivity,
+                    "w-1/4 flex items-center justify-center gap-8": true,
+                    "text-xl px-2 rounded-2xl": hasBackgroundActivity,
                 })}
                 style={{
                     backgroundColor: hasBackgroundActivity 
