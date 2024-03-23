58de755530959bcaac3b0364f8787651b90fe3e8
Eliran Shemesh <piro27@gmail.com>
2024-03-23 18:42:13 +0200

    Padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,8 +31,8 @@ export const Header = ({
         );
 
     return (
-        <div className="flex justify-between w-screen px-8 mt-4">
-            <div className="flex gap-8">
+        <div className="flex justify-between w-screen px-8">
+            <div className="flex mt-4 gap-8">
                 <House
                     size={32}
                     className="cursor-pointer"
@@ -50,7 +50,8 @@ export const Header = ({
             <div
                 className={classNames({
                     "w-fit flex items-center justify-center gap-8": true,
-                    "text-xl px-2 rounded-2xl": hasBackgroundActivity,
+                    "text-xl mt-2 py-2 px-4 rounded-2xl": hasBackgroundActivity,
+                    "mt-4": !hasBackgroundActivity
                 })}
                 style={{
                     backgroundColor: hasBackgroundActivity 
