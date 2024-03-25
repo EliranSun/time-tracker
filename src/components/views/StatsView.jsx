8ac91db6ae6812bf4da5406588975417439fb90f
Eliran Shemesh <piro27@gmail.com>
2024-03-25 23:58:15 +0200

    Update StatsView.jsx

--- src/components/views/StatsView.jsx
+++ src/components/views/StatsView.jsx
@@ -41,7 +41,7 @@ export const StatsView = ({activities}) => {
 
     return (
         <>
-            <div className="p-4 flex gap-2">
+            <div className="p-4 flex justify-between">
             <h1 className="text-3xl">{timeFrameName}</h1>
             <div className="flex items-center gap-2">
                 <CaretUp />
