93dce6ca19e039a17ac8faeb2c80edf74bf85fe4
Eliran Shemesh <piro27@gmail.com>
2024-03-26 00:29:10 +0200

    stylize

--- src/components/views/StatsView.jsx
+++ src/components/views/StatsView.jsx
@@ -42,7 +42,7 @@ export const StatsView = ({activities}) => {
     return (
         <div {...swipeHandlers}>
             <div className="px-4 py-2 flex justify-between">
-                <h1 className="text-3xl">{timeFrameName}</h1>
+                <h1 className="text-3xl font-mono">{timeFrameName.toUpperCase()}</h1>
                 <div className="flex gap-2">
                     <div className="flex items-center gap-2">
                         <CaretUp/>
