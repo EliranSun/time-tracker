875cf43b57592cab1dc2808616c0a6cbb6b18be5
Eliran Shemesh <piro27@gmail.com>
2024-04-03 09:07:04 +0300

    Update StatsView logic

--- src/components/views/StatsView.jsx
+++ src/components/views/StatsView.jsx
@@ -43,7 +43,8 @@ export const StatsView = ({activities}) => {
 
     const summedTime = useMemo(() => {
         const totalTimestamp = sortedActivities.reduce((acc, curr) => acc + curr.totalTime, 0);
-        return (totalTimestamp / 60 / 60);
+        // return Math.round(totalTimestamp / 60 / 60 / 1000);
+        return formatTimestamp(totalTimestamp);
     }, [sortedActivities]);
     
     const dateFrameName = Object.entries(Timeframes).find(([_key, value]) => value === timeFrame)[0];
