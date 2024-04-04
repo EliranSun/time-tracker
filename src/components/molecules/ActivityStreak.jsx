12112ade850951073d89e471f2ff53942a57cdfa
Eliran Shemesh <piro27@gmail.com>
2024-04-04 09:39:14 +0300

    Update streak logic

--- src/components/molecules/ActivityStreak.jsx
+++ src/components/molecules/ActivityStreak.jsx
@@ -1,19 +1,24 @@
 import {Badge} from "../atoms/Badge";
+import {useMemo} from "react";
 
 export const ActivityStreak = ({activities = []}) => {
-    const sortedByTime = activities.sort((a, b) => a.start - b.start);
-    let streak = 0;
-    let currentStreak = 0;
-    let lastEnd = 0;
-    sortedByTime.forEach(activity => {
-        if (activity.start - lastEnd < 24 * 60 * 60 * 1000) {
-            currentStreak++;
-            streak = Math.max(streak, currentStreak);
-        } else {
-            currentStreak = 1;
-        }
-        lastEnd = activity.end;
-    });
+    const streak = useMemo(() => {
+        const sortedByTime = activities.sort((a, b) => a.start - b.start);
+        let streak = 0;
+        let currentStreak = 0;
+        let lastEnd = 0;
+        sortedByTime.forEach(activity => {
+            if ((activity.start - lastEnd) < 24 * 60 * 60 * 1000) {
+                currentStreak++;
+                streak = Math.max(streak, currentStreak);
+            } else {
+                currentStreak = 0;
+            }
+            lastEnd = activity.end;
+        });
+
+        return streak;
+    }, [activities]);
 
     return <Badge value={streak} label="Streak"/>;
 };
