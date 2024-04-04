df44eeac4f0e2aef0a9031fd1db221c1a6c05828
Eliran Shemesh <piro27@gmail.com>
2024-04-04 09:48:36 +0300

    For cases where value is async, no UI shifting

--- src/components/atoms/Badge.jsx
+++ src/components/atoms/Badge.jsx
@@ -1,4 +1,4 @@
-export const Badge = ({label, value}) => {
+export const Badge = ({label, value = 0}) => {
     return (
         <span className="w-fit flex flex-col items-center justify-center gap-px">
             <span className="font-mono font-bold">{value}</span>
