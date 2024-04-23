b65ec0abcc7aced04df70a4761acbf7abfcf97af
Eliran Shemesh <piro27@gmail.com>
2024-04-23 12:32:02 +0300

    Do not split outside of zen mode

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ import {formatCounter} from "../utils/counter";
 export const Counter = ({isActive, lastStartTime, isZenMode}) => {
     return (
         <p className={classNames("font-mono", isZenMode ? "flex flex-col text-[15rem] leading-[10rem]" : "text-6xl")}>
-            {formatCounter(lastStartTime || 0).split(":").map((time, index) => {
+            {formatCounter(lastStartTime || 0).split(isZenMode ? ":" : "").map((time, index) => {
                 return (
                     <span key={index}>
                         {time}
