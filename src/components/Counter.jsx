1ae1656df09e1ae26d9943d604f39b483bbe0395
Eliran Shemesh <piro27@gmail.com>
2024-04-23 16:42:16 +0300

    Fix word break

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,13 +5,13 @@ export const Counter = ({isActive, lastStartTime, isZenMode}) => {
     // is active is important because we don't want to show the counter when switching activities, while one is active
     return (
         <p className={classNames("font-mono", isZenMode ? "flex flex-col text-[15rem] leading-[10rem]" : "text-6xl")}>
-            {isActive ? formatCounter(lastStartTime || 0).split(isZenMode ? ":" : "").map((time, index) => {
+            {formatCounter(isActive ? 0 : lastStartTime || 0).split(isZenMode ? ":" : "").map((time, index) => {
                 return (
                     <span key={index}>
                         {time}
                     </span>
                 )
-            }) : "00:00:00"}
+            })}
         </p>
     )
 };
