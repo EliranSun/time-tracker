0d0d64be2cc2eaa3dae56fda01acf0515001cad6
Eliran Shemesh <piro27@gmail.com>
2024-04-23 16:57:34 +0300

    A bit wider leading

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ import {formatCounter} from "../utils/counter";
 export const Counter = ({isActive, lastStartTime, isZenMode}) => {
     // is active is important because we don't want to show the counter when switching activities, while one is active
     return (
-        <p className={classNames("font-mono", isZenMode ? "flex flex-col text-[15rem] leading-[10rem]" : "text-6xl")}>
+        <p className={classNames("font-mono", isZenMode ? "flex flex-col text-[15rem] leading-[11rem]" : "text-6xl")}>
             {formatCounter(isActive ? lastStartTime || 0 : 0).split(isZenMode ? ":" : "").map((time, index) => {
                 return (
                     <span key={index}>
