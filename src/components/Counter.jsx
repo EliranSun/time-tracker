baf22c10e971e8c3c9bb6b84d4d10d7c8d3d94a5
Eliran Shemesh <piro27@gmail.com>
2024-04-24 10:16:19 +0300

    Adjust design

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,13 +4,16 @@ import {formatCounter} from "../utils/counter";
 export const Counter = ({isActive, lastStartTime, isZenMode}) => {
         // is active is important because we don't want to show the counter when switching activities, while one is active
         return (
-            <p className={classNames("font-mono", isZenMode ? "flex flex-col text-[15rem] leading-[12rem]" : "text-6xl")}>
+            <p className={classNames("font-mono", {
+                "flex flex-col text-[15rem] leading-[11.2rem]": isZenMode,
+                "text-6xl": !isZenMode,
+            })}>
                 {formatCounter(isActive ? lastStartTime || 0 : 0).split(isZenMode ? ":" : "").map((time, index) => {
                     return (
                         <span key={index} className={classNames({
                             "opacity-80": index === 0,
-                            "opacity-50": index === 1,
-                            "opacity-20": index === 2,
+                            "opacity-60": index === 1,
+                            "opacity-30": index === 2,
                         })}>
                         {time}
                     </span>
