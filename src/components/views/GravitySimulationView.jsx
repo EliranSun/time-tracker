8c6e561cb129bcbee6e23647eba9602986f5b3fe
Eliran Shemesh <piro27@gmail.com>
2024-05-03 15:52:14 +0300

    wip

--- src/components/views/GravitySimulationView.jsx
+++ src/components/views/GravitySimulationView.jsx
@@ -1,109 +1,95 @@
-import React, {useRef, useCallback} from 'react';
+import React, {useRef, useCallback, useEffect, useMemo, useState} from 'react';
 import Matter from 'matter-js';
 
-const GravitySimulation = ({ counter }) => {
-    // Ref for the container to add the divs
-    const sceneRef = useRef(null);
-    const engineRef = useRef(Matter.Engine.create());
-    const permission = useCallback(() => {
-        if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
-            DeviceMotionEvent.requestPermission()
-                .then(response => {
-                    if (response === "granted") {
-                        const {Engine, Render, Bodies, World} = Matter;
+const grantMotionPermission = async () => {
+    if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
+        const response = await DeviceMotionEvent.requestPermission();
+        return response === "granted";
+    } else {
+        return false;
+    }
+};
 
-                        const createBall = () => {
-                            const x = window.innerWidth / 2;
-                            const y = 0; // start at the top of the screen
-                            const radius = 5;
-                            return Bodies.circle(x, y, radius, {
-                                restitution: 0.8,
-                                render: {
-                                    fillStyle: "white",
-                                    strokeStyle: "black",
-                                    lineWidth: 2,
-                                }
-                            });
-                        };
-
-                        // Create an engine
-                        engineRef.current = Engine.create();
-
-                        // Create a renderer
-                        const render = Render.create({
-                            element: sceneRef.current,
-                            engine: engineRef.current,
-                            options: {
-                                width: window.innerWidth,
-                                height: window.innerHeight,
-                                wireframes: false,
-                                background: "transparent",
-                                showVelocity: true,
-                                showAngleIndicator: true
-                            },
-                        });
-
-                        // Create a bunch of rectangles (divs) with random positions and add them to the world
-                        const divBodies = [];
-
-                        engineRef.current.gravity.y = 1;
-
-                        const wallsOptions = {
-                            isStatic: true,
-                            restitution: 0.8,
-                            render: {
-                                fillStyle: "transparent",
-                                strokeStyle: "transparent",
-                            }
-                        };
-
-                        const floor = Bodies.rectangle(400, 610, 810, 60, wallsOptions);
-                        const wallLeft = Bodies.rectangle(0, 300, 60, 600, wallsOptions);
-                        const wallRight = Bodies.rectangle(window.innerWidth, 300, 60, 600, wallsOptions);
-
-                        World.add(engineRef.current.world, wallLeft);
-                        World.add(engineRef.current.world, wallRight);
-                        World.add(engineRef.current.world, divBodies);
-                        World.add(engineRef.current.world, floor);
-
-                        // Running the engine
-                        Engine.run(engineRef.current);
-
-                        // Running the renderer
-                        Render.run(render);
-
-                        // window.addEventListener("devicemotion", (event) => {
-                        //     const {accelerationIncludingGravity} = event;
-                        //
-                        //     // Use accelerationIncludingGravity.x and accelerationIncludingGravity.y to adjust gravity
-                        //     // You might need to scale these values to fit your simulation's needs
-                        //     engineRef.current.world.gravity.x = accelerationIncludingGravity.x * 0.2;
-                        //     engineRef.current.world.gravity.y = -accelerationIncludingGravity.y * 0.2;
-                        // });
-
-                        setInterval(() => {
-                            const ball = createBall();
-                            World.add(engineRef.current.world, ball);
-                        }, 1000);
-                    } else {
-                        alert(`Permission denied: ${response}`);
-                    }
-                })
-                .catch(error => {
-                    alert(`Error occurred: ${error}`);
-                })
-        } else {
-            alert("DeviceMotionEvent is not defined");
+const createBall = (Bodies) => {
+    const x = window.innerWidth / 2;
+    const y = 0; // start at the top of the screen
+    const radius = 5;
+    return Bodies.circle(x, y, radius, {
+        restitution: 0.8,
+        render: {
+            fillStyle: "white",
+            strokeStyle: "black",
+            lineWidth: 2,
         }
-    }, []);
+    });
+};
+
+const createAndGetWorldInstance = (element) => {
+    const {Engine, Render, Bodies, World} = Matter;
+    const engine = Engine.create();
+    const render = Render.create({
+        element,
+        engine,
+        options: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            wireframes: false,
+            background: "transparent",
+            showVelocity: true,
+            showAngleIndicator: true
+        },
+    });
+
+    const wallsOptions = {
+        isStatic: true,
+        restitution: 0.8,
+        render: {
+            fillStyle: "transparent",
+            strokeStyle: "transparent",
+        }
+    };
+
+    const floor = Bodies.rectangle(400, 610, 810, 60, wallsOptions);
+    const wallLeft = Bodies.rectangle(0, 300, 60, 600, wallsOptions);
+    const wallRight = Bodies.rectangle(window.innerWidth, 300, 60, 600, wallsOptions);
+
+    World.add(engine.world, wallLeft);
+    World.add(engine.world, wallRight);
+    World.add(engine.world, floor);
+
+    Engine.run(engine);
+    Render.run(render);
+
+    return {engine, bodies: Bodies, world: World};
+}
+
+const GravitySimulation = ({counter}) => {
+    const sceneRef = useRef(null);
+    const [isGranted, setIsGranted] = useState(false);
+    const [bodies, setBodies] = useState([]);
 
     return (
         <>
             <button
                 className="absolute top-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded user-select-none hover:bg-blue-700"
-                onClick={permission}>
+                onClick={async () => {
+                    const isGranted = await grantMotionPermission();
+                    if (!isGranted) {
+                        return;
+                    }
+
+                    const {engine, bodies, world} = createAndGetWorldInstance(sceneRef.current);
+                    const ball = createBall(bodies);
+                    world.add(engine.world, ball);
+                }}>
                 Apply gravity
             </button>
+            {isGranted ? <button onClick={() => {
+                // const ball = createBall(bodies);
+                // world.add(engine.world, ball);
+            }}>
+                ADD BALL
+            </button> : null}
             <div ref={sceneRef}/>
         </>
     )
