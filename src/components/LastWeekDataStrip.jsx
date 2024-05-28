8b1041b317cbfaa50b68c141d64fe3348149945b
Eliran Shemesh <piro27@gmail.com>
2024-05-28 08:02:18 +0300

    Update LastWeekDataStrip.jsx

--- src/components/LastWeekDataStrip.jsx
+++ src/components/LastWeekDataStrip.jsx
@@ -20,11 +20,8 @@ export const LastWeekDataStrip = ({activity, data}) => {
     }, [activity.name, data]);
 
     return (
-        <div>
-            hi
             <div className="absolute overflow-hidden w-full mt-28 z-50">
                 <WeeklyLineChart data={lastWeekData.data.map(item => ({...item, name: item.name.slice(0, 1)}))}/>
             </div>
-        </div>
     );
 };
