f077163c14a27bd69337418a3cc1dd06e57cc7f0
Eliran Shemesh <piro27@gmail.com>
2024-05-28 10:45:29 +0300

    Fix brush

--- src/components/atoms/WeeklyLineChart.jsx
+++ src/components/atoms/WeeklyLineChart.jsx
@@ -12,40 +12,20 @@ import {
     Brush
 } from 'recharts';
 
-// Sample data
-// const data = [
-//     {name: 'S', h: 3},
-//     {name: 'M', h: 2},
-//     {name: 'T', h: 1},
-//     {name: 'W', h: 1.5},
-//     {name: 'T', h: 4},
-//     {name: 'F', h: 5},
-//     {name: 'S', h: 1},
-//     {name: 'S', h: 3},
-//     {name: 'M', h: 2},
-//     {name: 'T', h: 1},
-//     {name: 'W', h: 1.5},
-//     {name: 'T', h: 4},
-//     {name: 'F', h: 5},
-//     {name: 'S', h: 1},
-// ];
-
 export const WeeklyLineChart = ({data = []}) => {
-    const [brushEndIndex, setBrushEndIndex] = useState(data.length);
-
     return (
-        <ResponsiveContainer width={window.innerWidth * 0.8} height={150}>
-            <ComposedChart data={data}>
+        <ResponsiveContainer width="100%" height="100%">
+            <ComposedChart data={data} width={500} height={400}>
                 <CartesianGrid stroke="#f5f5f5"/>
                 <XAxis dataKey="dayName"/>
                 <YAxis/>
+                <Tooltip/>
                 <Legend/>
                 <Line type="basis" dataKey="duration" stroke="black" dot={false}/>
                 <Brush
                     dataKey="dayName"
                     startIndex={data.length - 10}
-                    endIndex={brushEndIndex}
-                    onChange={(e) => setBrushEndIndex(e.endIndex)}
+                    endIndex={data.length - 1}
                 />
             </ComposedChart>
         </ResponsiveContainer>
