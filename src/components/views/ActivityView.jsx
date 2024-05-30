1c81326debf89c8ba3ee5790927042a25e2478a5
Eliran Shemesh <piro27@gmail.com>
2024-05-30 10:17:12 +0300

    Debug

--- src/components/views/ActivityView.jsx
+++ src/components/views/ActivityView.jsx
@@ -156,6 +156,9 @@ export const ActivityView = ({
                         <LastWeekDataStrip
                             data={dayByDayData}
                             activity={activity}/>
+                        <pre className="h-60 overflow-auto">
+                            {JSON.stringify(dayByDayData, null, 2)}
+                        </pre>
                     </Block>
                 </div>
             </div>
