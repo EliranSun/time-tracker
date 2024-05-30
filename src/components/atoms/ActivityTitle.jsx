5966846a19086dbb15dd7f855e669f6f3145c028
Eliran Shemesh <piro27@gmail.com>
2024-05-30 08:03:02 +0300

    Action based title

--- src/components/atoms/ActivityTitle.jsx
+++ src/components/atoms/ActivityTitle.jsx
@@ -1,13 +1,15 @@
 import classNames from "classnames";
 
 export const ActivityTitle = ({name, isZenMode}) => {
+    const actionBasedTitle = name.replace("ive", "e");
+    
     return (
         <h1
             className={classNames("break-words w-fit font-mono text-center overflow-hidden h-fit", {
                 "tracking-[-14px] pr-4 -ml-2 leading-tight font-thin text-8xl": !isZenMode,
                 "tracking-[-5px] font-extrabold h-full text-7xl": isZenMode,
             })}>
-            {name.slice(0,6).toUpperCase()}
+            {actionBasedTitle.slice(0,6).toUpperCase()}
         </h1>
     )
 }
